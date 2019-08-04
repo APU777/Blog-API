d73b3d09f3a13425ac9c150848383ffea5db1777
Vitaliy Alexandrovich <Dobrota2017@outlook.com>
2019-08-04 17:15:48 +0300

    Initial commit

---
+++ Models/Comments.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Models
+{
+    public class Comments
+    {
+        public string Author { get; set; }
+        public string Body { get; set; }
+        public string CommentId { get; set; }
+        public DateTime CreatDate { get; set; }
+        public string PostId { get; set; }
+    }
+}
